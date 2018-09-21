394362b3f8ef7c047f4f2177e77241915b7b1c4e
Cory Redmond <ace@ac3-servers.eu>
2018-09-21 19:49:00 +0100

    Sleep 23 ms in stead of 10 to decrease CPU usage

--- cmd/LightPlease/main.go
+++ cmd/LightPlease/main.go
@@ -136,7 +136,7 @@ func main() {
 			}
 		}
 
-		time.Sleep(10 * time.Millisecond) // Sleep so we don't crash the API.
+		time.Sleep(23 * time.Millisecond) // Sleep so we don't crash the API.
 	}
 
 }
